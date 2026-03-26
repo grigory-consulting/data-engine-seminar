61b72ed7689877ea9c5bff6397b5238790f64105
grigory-consulting <consulting@vadze.de>
2026-03-26 09:27:25 +0100

    copy-paste

---
+++ earthquake/dags_copy_paste/fct_avg_earthquake.py
@@ -0,0 +1,148 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+
+# DAG-Konfiguration
+OWNER = "g.devadze"
+DAG_ID = "fct_avg_day_earthquake"
+
+# Verwendete Tabellen im DAG
+LAYER = "raw"
+SOURCE = "earthquake"
+SCHEMA = "dm"
+TARGET_TABLE = "fct_avg_day_earthquake"
+
+# DWH
+PG_CONNECT = "postgres_dwh"
+
+LONG_DESCRIPTION = """
+# LONG DESCRIPTION
+"""
+
+SHORT_DESCRIPTION = "SHORT DESCRIPTION"
+
+args = {
+    "owner": OWNER,
+    "retries": 3,
+    "retry_delay": pendulum.duration(hours=1),
+}
+
+
+with DAG(
+    dag_id=DAG_ID,
+    schedule_interval="0 5 * * *",
+    start_date=pendulum.datetime(2026, 2, 14, tz="Europe/Berlin"),
+    catchup=False,
+    default_args=args,
+    tags=["dm", "pg"],
+    description=SHORT_DESCRIPTION,
+    concurrency=1,
+    max_active_tasks=1,
+    max_active_runs=1,
+) as dag:
+    dag.doc_md = LONG_DESCRIPTION
+
+    start = EmptyOperator(
+        task_id="start",
+    )
+
+    sensor_on_raw_layer = ExternalTaskSensor(
+        task_id="sensor_on_raw_layer",
+        external_dag_id="raw_from_s3_to_pg",
+        allowed_states=["success"],
+        mode="reschedule",
+        timeout=360000,  # Laufzeit des Sensors
+        poke_interval=60,  # Pruefintervall
+    )
+
+    prepare_dm_objects = SQLExecuteQueryOperator(
+        task_id="prepare_dm_objects",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        CREATE SCHEMA IF NOT EXISTS stg;
+        CREATE SCHEMA IF NOT EXISTS {SCHEMA};
+
+        CREATE TABLE IF NOT EXISTS {SCHEMA}.{TARGET_TABLE}
+        (
+            date date,
+            avg double precision
+        )
+        """,
+    )
+
+    drop_stg_table_before = SQLExecuteQueryOperator(
+        task_id="drop_stg_table_before",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    create_stg_table = SQLExecuteQueryOperator(
+        task_id="create_stg_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        CREATE TABLE stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}" AS
+        SELECT
+            time::date AS date,
+            avg(mag::float)
+        FROM
+            ods.fct_earthquake
+        WHERE
+            time::date = '{{{{ data_interval_start.format('YYYY-MM-DD') }}}}'
+        GROUP BY 1
+        """,
+    )
+
+    drop_from_target_table = SQLExecuteQueryOperator(
+        task_id="drop_from_target_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DELETE FROM {SCHEMA}.{TARGET_TABLE}
+        WHERE date IN
+        (
+            SELECT date FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        )
+        """,
+    )
+
+    insert_into_target_table = SQLExecuteQueryOperator(
+        task_id="insert_into_target_table",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        INSERT INTO {SCHEMA}.{TARGET_TABLE}
+        SELECT * FROM stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    drop_stg_table_after = SQLExecuteQueryOperator(
+        task_id="drop_stg_table_after",
+        conn_id=PG_CONNECT,
+        autocommit=True,
+        sql=f"""
+        DROP TABLE IF EXISTS stg."tmp_{TARGET_TABLE}_{{{{ data_interval_start.format('YYYY-MM-DD') }}}}"
+        """,
+    )
+
+    end = EmptyOperator(
+        task_id="end",
+    )
+
+    (
+            start >>
+            sensor_on_raw_layer >>
+            prepare_dm_objects >>
+            drop_stg_table_before >>
+            create_stg_table >>
+            drop_from_target_table >>
+            insert_into_target_table >>
+            drop_stg_table_after >>
+            end
+    )
